7e77319b3892a80ac3c5b8f11cb3577bd68f3884
wisnu putra dewa <com.owl.minerva@gmail.com>
2020-01-15 12:13:44 +0700

    push 15/01/2020 12.13am

--- app/src/main/java/com/mobile/fkpn/model/MapConverter.kt
+++ app/src/main/java/com/mobile/fkpn/model/MapConverter.kt
@@ -1,7 +1,7 @@
 package com.mobile.fkpn.model
 
 class MapConverter {
-    public fun convert(hashMap: HashMap<String, String>): String {
+    fun convert(hashMap: HashMap<String, String>): String {
         return hashMap.toString().replace(", ", "&").replace("{", "").replace("}", "")
     }
 }
--- app/src/main/java/com/mobile/fkpn/model/Url.java
+++ app/src/main/java/com/mobile/fkpn/model/Url.java
@@ -2,9 +2,9 @@ package com.mobile.fkpn.model;
 
 public class Url {
     public static String get() {
-        return "https://www.padippob.com/api/";
+        return "http://hasma.id/fkpn/api/";
     }
     public static String getBinary() {
-        return "https://www.padippob.com/";
+        return "http://hasma.id/fkpn/";
     }
 }
